ee456184c1317102fd358d6ecf3216fdcc93bf97
victor herane <victorhugoherane@gmail.com>
2024-06-18 11:16:35 -0400

    aad

--- src/components/Repositories/RepositoriesTable.tsx
+++ src/components/Repositories/RepositoriesTable.tsx
@@ -6,20 +6,34 @@ type RepositoriesTableProps = {
   repositories: Repository[];
 };
 
-export default function RepositoriesTable({ repositories, onClickRepository }: RepositoriesTableProps) {
+export default function RepositoriesTable({
+  repositories,
+  onClickRepository,
+}: RepositoriesTableProps) {
   return (
     <div className="max-w-[380px] overflow-x-auto lg:overflow-hidden lg:max-w-full lg:w-full">
       <table className="w-full bg-white rounded-md">
         <thead>
           <tr className="bg-gray-200">
-            <th className="py-2 px-4 border-b border-gray-200 text-start rounded-tl-[8px] min-w-[200px] lg:min-w-[280px] ">Nome</th>
-            <th className="py-2 px-4 border-b border-gray-200 text-start">Proprietário</th>
-            <th className="py-2 px-4 border-b border-gray-200 text-start rounded-tr-[8px] min-w-[300px]">Descrição</th>
+            <th className="py-2 px-4 border-b border-gray-200 text-start rounded-tl-[8px] min-w-[200px] lg:min-w-[280px] ">
+              Nome do Repositório
+            </th>
+            <th className="py-2 px-4 border-b border-gray-200 text-start">
+              Dono do Repositório
+            </th>
+            <th className="py-2 px-4 border-b border-gray-200 text-start rounded-tr-[8px] min-w-[300px]">
+              Descrição
+            </th>
           </tr>
         </thead>
         <tbody>
           {repositories.map((repo, index) => (
-            <RepositoryItem key={repo.id} repo={repo} index={index} onClickRepository={onClickRepository} />
+            <RepositoryItem
+              key={repo.id}
+              repo={repo}
+              index={index}
+              onClickRepository={onClickRepository}
+            />
           ))}
         </tbody>
       </table>
--- src/components/RepositoryDetails/Modal.tsx
+++ src/components/RepositoryDetails/Modal.tsx
@@ -8,7 +8,12 @@ interface ModalProps {
   isLoading: boolean;
 }
 
-export default function Modal({ isOpened, repositoryDetails, onClose, isLoading }: ModalProps) {
+export default function Modal({
+  isOpened,
+  repositoryDetails,
+  onClose,
+  isLoading,
+}: ModalProps) {
   const handleRepositoryOwnerName = () => {
     if (!repositoryDetails.owner) {
       return "";
@@ -29,17 +34,27 @@ export default function Modal({ isOpened, repositoryDetails, onClose, isLoading 
         isOpened ? "flex" : "hidden"
       } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full`}
     >
-      {isOpened && <div className="fixed inset-0 bg-black opacity-50 z-40"></div>}
+      {isOpened && (
+        <div className="fixed inset-0 bg-black opacity-50 z-40"></div>
+      )}
       <div className="relative p-4 w-full max-w-2xl max-h-full z-50">
         <div className="relative bg-gray-100 rounded-lg shadow dark:bg-gray-300">
           <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-400">
-            <h3 className="text-xl font-semibold ">Detalhes do repositório {isLoading ? "" : repositoryDetails?.name}</h3>
+            <h3 className="text-xl font-semibold ">
+              Detalhes do repositório {isLoading ? "" : repositoryDetails?.name}
+            </h3>
             <button
               type="button"
               className="text-gray-500 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
               onClick={onClose}
             >
-              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
                 <path
                   stroke="currentColor"
                   strokeLinecap="round"
@@ -58,13 +73,25 @@ export default function Modal({ isOpened, repositoryDetails, onClose, isLoading 
             </div>
           ) : (
             <div className="p-4 md:p-5 space-y-4">
-              <p className="text-base leading-relaxed ">Proprietário: {handleRepositoryOwnerName()}</p>
               <p className="text-base leading-relaxed ">
-                Total de commits: {repositoryDetails?.defaultBranchRef?.target?.history?.totalCount}
+                Dono do Repo: {handleRepositoryOwnerName()}
               </p>
-              <p className="text-base leading-relaxed ">Issues abertas: {repositoryDetails?.issues?.totalCount}</p>
-              <p className="text-base leading-relaxed ">Pull requests: {repositoryDetails?.pullRequests?.totalCount}</p>
-              <p className="text-base leading-relaxed ">Total de estrelas: {repositoryDetails?.stargazers?.totalCount}</p>
+              <p className="text-base leading-relaxed ">
+                Total de commits:{" "}
+                {
+                  repositoryDetails?.defaultBranchRef?.target?.history
+                    ?.totalCount
+                }
+              </p>
+              <p className="text-base leading-relaxed ">
+                Issues abertas: {repositoryDetails?.issues?.totalCount}
+              </p>
+              <p className="text-base leading-relaxed ">
+                Pull requests: {repositoryDetails?.pullRequests?.totalCount}
+              </p>
+              <p className="text-base leading-relaxed ">
+                Total de estrelas: {repositoryDetails?.stargazers?.totalCount}
+              </p>
             </div>
           )}
         </div>
